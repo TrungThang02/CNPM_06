eabf370ad447faa587146bfa68ceb9ddc981baeb
TrungThang02 <80338828+TrungThang02@users.noreply.github.com>
2023-06-06 08:47:33 +0700

    Create SumTwoNumbers.js

---
+++ SumTwoNumbers.js
@@ -0,0 +1,14 @@
+// Function to calculate the sum of two numbers
+function SumTwoNumbers(number1, number2) {
+  // Initialize the sum variable
+  let sum = 0;
+
+  // Calculate the sum of number1 and number2
+  sum = number1 + number2;
+
+  // Output the result to the console
+  console.log(sum);
+}
+
+// Call the SumTwoNumbers function with arguments 2 and 3
+SumTwoNumbers(2, 3);
